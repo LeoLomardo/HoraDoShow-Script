6e7446bd6665262f6732067d7bcf3deb55195931
Matheus-Valejo <matheusvalejo8@gmail.com>
2023-11-24 16:05:39 -0300

    testes

--- exemplo1.c
+++ exemplo1.c
@@ -9,23 +9,15 @@ int main(void) {
 	printf("RECEBA [XX]: ");
 	scanf("%d", &XX); 
  
- 	int A;
-	printf("RECEBA [A]: ");
-	scanf("%d", &A); 
- 
  	int Z;
-	X = 0;
-	X = X << 10 ;
-	Z = X;
-
-	if	(X > 3){
-
-	Z = Z - 4 ;
-	Z = Z;
-
-	} else {
-
 	Z = 0;
+	Z = Z + 10 ;
+	Z = Z;
+	for (int i = 0; i < 3; i++) {
+
+	Z = Z - 1 ;
+	Z = Z - 1 ;
+	Z = Z - 1 ;
 
 	}
 
